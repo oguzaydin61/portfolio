f1a7f24ec98bb7fa81f507ea4a5c531c17575439
oguzaydin61 <oguzhanaydin476@gmail.com>
2026-01-11 14:23:54 +0300

    add portfolio project

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,79 @@
+import { Suspense, useEffect, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useFBX, useGLTF } from "@react-three/drei"
+import CanvasLoader from '../Loader'
+
+
+const Computers = ({isMobile}) => {
+  const car = useGLTF('./head/scene.gltf')
+  return (
+    <mesh>
+    <hemisphereLight intensity={.8} groundColor='black' />
+    <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}
+    />
+    <pointLight intensity={1} />
+    <primitive
+      object={car.scene}
+      scale={isMobile ? 0.7 : 0.05}
+      //position ile dairesel dönüş ayarlanıyor muhtemelen x ile
+      position={isMobile ? [0, -3, -2.2] : [0, -0.3, -0]}
+      autoRotate={false}
+      rotation={[-0.01, 0, -0.0]}
+    />
+  </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+ const [isMobile, setIsMobile] = useState(false);
+ useEffect(() => {
+   const mediaQuery = window.matchMedia('(max-width:500px)');
+   setIsMobile(mediaQuery.matches);
+
+   const handleMediaQueryChange = (event) => {
+    setIsMobile(event.matches);
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return() => {
+      mediaQuery.removeEventListener('change',handleMediaQueryChange);
+    }
+   }
+ 
+   
+ }, [])
+ 
+
+
+  //camera ile ayarla
+  return (
+    <Canvas
+      frameloop="demand" shadows camera={{ position: [0, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false}
+         maxPolarAngle={Math.PI / 2}
+         minPolarAngle={Math.PI / 2}
+
+          autoRotate={true}
+          autoRotateSpeed={1}
+          
+          
+          
+
+        />
+        <Computers isMobile={isMobile}/>
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+
+}
+
+export default ComputersCanvas
---
+++ src/constants/index.js
@@ -0,0 +1,251 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    curator,
+    trust,
+    eksim
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Node JS Developer",
+      icon: backend,
+    },
+    {
+      title: "Flutter Developer",
+      icon: mobile,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Intern .Net Developer",
+      company_name: "Eksim Invesment Holding",
+      icon: eksim,
+      iconBg: "#003049",
+      date: "June 2021 - September 2021",
+      points: [
+        "Developing and maintaining web applications using .Net Core and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Intern Unity Developer",
+      company_name: "Trust Games",
+      icon: trust,
+      iconBg: "#fdf0d5",
+      date: "Jan 2022 - September 2022",
+      points: [
+        "Unity Game Development Projects: Actively contributed to captivating VR game projects, implementing interactive mechanics and impressive graphics.",
+        "Collaborative Teamwork: Excelled in a team environment, collaborating with developers, artists, and designers on VR projects using Git.",
+        "Problem Solving and Debugging: Resolved challenges in VR development, optimized performance, and improved the overall VR gameplay experience.",
+        "Learning and Growth: Expanded skills in C#, particle systems, shaders, and VR development through workshops and hands-on projects.",
+      ],
+    },
+    {
+      title: "Unity & Web Developer",
+      company_name: "Curator Studios",
+      icon: curator,
+      iconBg: "#003049",
+      date: "September 2022 - May 2023",
+      points: [
+        "Web Development with JavaScript: Spearheaded the creation of the company's website using JavaScript, applying modern web design principles to deliver an engaging and responsive user experience.",
+        "Server-side Development with Node.js: Played a significant role in server-side development, utilizing Node.js to build robust APIs and backend systems that supported the web3 games' functionality.",
+        "Web3 Game Interface Development: Led the design and development of captivating web3 game interfaces, seamlessly integrating blockchain functionalities to enhance user engagement and experience.",
+        "Website Optimization and Performance: Ensured the website's optimal performance, employing optimization techniques and best practices to achieve fast loading times and a smooth browsing experience..",
+        "Adapting to Emerging Technologies: Demonstrated adaptability in the ever-evolving web3 landscape, staying up-to-date with the latest Node.js and web development trends to drive innovation in the projects."
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
